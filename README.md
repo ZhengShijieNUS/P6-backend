92bba104ea0e7f57b121651f996e664f07173c0f
ZhengShijieNUS <ku382168215@gmail.com>
2023-09-27 22:09:35 +0800

    milestone1: creating empty Node.js server project, add Express.js and Mongoose, successfully send get/post request and connect MongoDB Altas

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # P6-backend
 This is a backend development learning project with Node.js and its framework Express.js
+
+
+# How to run the project
+execute "npm run nodeserver" to run the project with ES module
---
+++ src/app.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const app = express()
+
+app.use(express.json())
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:QdtDsxu5tNjbQYjJ@mongodb-learning.ayd34ej.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!')
+  })
+  .catch(error => {
+    console.log('Unable to connect to MongoDB Atlas!')
+    console.error(error)
+  })
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
+
+app.use('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'My first thing',
+      description: 'All of the info about my first thing',
+      imageUrl: '',
+      price: 4900,
+      userId: 'qsomihvqios'
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'My second thing',
+      description: 'All of the info about my second thing',
+      imageUrl: '',
+      price: 2900,
+      userId: 'qsomihvqios'
+    }
+  ]
+  res.status(200).json(stuff)
+})
+
+
+app.get('/get', (req, res, next) => {
+    res.status(200).json({result: 'get success'})
+})
+
+app.post('/post', (req, res, next) => {
+    const result = req.body
+    result.result = "success post"
+    res.status(201).json(result)
+})
+
+export default app
