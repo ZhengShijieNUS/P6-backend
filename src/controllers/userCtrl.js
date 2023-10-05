ae885716bec083ac7f5b6a27f69bf3bebebf8a6f
ZhengShijieNUS <ku382168215@gmail.com>
2023-10-05 14:23:00 +0800

    milestone2: add user route, complete signup and login function

--- src/controllers/userCtrl.js
+++ src/controllers/userCtrl.js
@@ -1,27 +1,65 @@
 import User from '../models/user.js'
+import bcrypt from 'bcrypt'
+import jsonwebtoken from 'jsonwebtoken'
 
 export function signup (req, res, next) {
-  console.log('user signup successful')
-  User.find()
-    .then(() => {
-      res.status(200).json({
-        result: 'signup success'
-      })
-    })
-    .catch(err => {
-      res.status(400).json({ error: err })
-    })
+  bcrypt.hash(req.body.password, 10).then(
+    (hash) => {
+        const user = new User({
+            email: req.body.email,
+            password: hash
+        });
+        user.save().then(() => {
+            res.status(201).json({
+                message: 'User added successfully!'
+            })
+        }).catch(err => {
+            res.status(500).json({
+                err:err
+            });
+        });
+    }
+  )
 }
 
 export function login (req, res, next) {
-  console.log('user login successful')
-  User.find()
-    .then(() => {
-      res.status(200).json({
-        result: 'login success'
-      })
+
+  User.findOne({email: req.body.email}).then(
+    (user) => {
+        if(!user){
+            return res.status(401).json({
+                error: new Error('User not found')
+            })
+        }
+
+        bcrypt.compare(req.body.password, user.password).then(
+            (valid) => {
+                if(!valid){
+                    return res.status(401).json({
+                        error: new Error('Incorrect password')
+                    })
+                }
+
+                const token = jsonwebtoken.sign(
+                    {userId: user._id},
+                    'RANDOM_TOKEN_SECRET',
+                    {expiresIn: '24h'}
+                )
+
+                res.status(200).json({
+                    userId: user._id,
+                    token: token
+                })
+            }
+        ).catch(err => {
+            res.status(500).json({
+                err: err
+            })
+        })
+    }
+  ).catch(err => {
+    res.status(500).json({
+        err:err
     })
-    .catch(err => {
-      res.status(400).json({ error: err })
-    })
+  })
 }
