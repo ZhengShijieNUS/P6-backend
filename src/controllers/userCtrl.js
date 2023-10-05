f4161aa70899eb302c35fc02f95e800bd5dac6f8
ZhengShijieNUS <ku382168215@gmail.com>
2023-10-05 17:42:37 +0800

    milestone2: rewrite code using async/await keyword

--- src/controllers/userCtrl.js
+++ src/controllers/userCtrl.js
@@ -2,93 +2,80 @@ import User from '../models/user.js'
 import bcrypt from 'bcrypt'
 import jsonwebtoken from 'jsonwebtoken'
 
-export function signup (req, res, next) {
-  bcrypt.hash(req.body.password, 10).then(hash => {
+export async function signup (req, res, next) {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
     const user = new User({
       email: req.body.email,
-      password: hash
+      password: hashedPassword
     })
-    user
-      .save()
-      .then(() => {
-        res.status(201).json({
-          message: 'User added successfully!'
-        })
-      })
-      .catch(err => {
-        res.status(500).json({
-          err: err
-        })
-      })
-  })
+
+    await user.save()
+    res.status(201).json({
+      message: 'User added successfully!'
+    })
+  } catch (err) {
+    res.status(500).json({
+      err: err.message
+    })
+  }
 }
 
-export function login (req, res, next) {
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({
-          error: new Error('User not found').message
-        })
-      }
+export async function login (req, res, next) {
+  try {
+    const user = await User.findOne({ email: req.body.email })
 
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({
-              error: new Error('Incorrect password')
-            })
-          }
-
-          const token = jsonwebtoken.sign(
-            { userId: user._id },
-            'RANDOM_TOKEN_SECRET',
-            { expiresIn: '24h' }
-          )
-
-          res.status(200).json({
-            userId: user._id,
-            token: token
-          })
-        })
-        .catch(err => {
-          res.status(500).json({
-            err: err
-          })
-        })
-    })
-    .catch(err => {
-      res.status(500).json({
-        err: err
+    if (!user) {
+      return res.status(404).json({
+        error: new Error('User not found').message
       })
+    }
+
+    const valid = await bcrypt.compare(req.body.password, user.password)
+
+    if (!valid) {
+      return res.status(401).json({
+        error: new Error('Invalid password').message
+      })
+    }
+
+    const token = jsonwebtoken.sign(
+      { userId: user._id },
+      'RANDOM_TOKEN_SECRET',
+      {
+        expiresIn: '24h'
+      }
+    )
+
+    res.status(200).json({
+      userId: user._id,
+      token: token
     })
+  } catch (err) {
+    res.status(500).json({
+      err: err.message
+    })
+  }
 }
 
-export function removeUser (req, res, next) {
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(404).json({
-          error: new Error('User not found').message
-        })
-      }
+export async function removeUser (req, res, next) {
+  try {
+    const user = await User.findOne({ email: req.body.email })
 
-      User.deleteOne({ email: req.body.email })
-        .then(() => {
-          res.status(200).json({
-            message: 'User deleted successfully'
-          })
-        })
-        .catch(err => {
-          res.status(400).json({
-            err: err
-          })
-        })
-    })
-    .catch(err => {
-      res.status(400).json({
-        err: err.message
+    if (!user) {
+      return res.status(404).json({
+        error: 'User not found'
       })
+    }
+
+    await User.deleteOne({ email: req.body.email })
+
+    res.status(200).json({
+      message: 'User deleted successfully'
     })
+  } catch (err) {
+    res.status(400).json({
+      error: err.message
+    })
+  }
 }
