d57a7869c1254d12b56c762b5d3c9b56d6ad09f6
ZhengShijieNUS <ku382168215@gmail.com>
2023-10-05 15:25:30 +0800

    milestone2: add reove user function for internal use

--- src/controllers/userCtrl.js
+++ src/controllers/userCtrl.js
@@ -3,63 +3,92 @@ import bcrypt from 'bcrypt'
 import jsonwebtoken from 'jsonwebtoken'
 
 export function signup (req, res, next) {
-  bcrypt.hash(req.body.password, 10).then(
-    (hash) => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save().then(() => {
-            res.status(201).json({
-                message: 'User added successfully!'
-            })
-        }).catch(err => {
-            res.status(500).json({
-                err:err
-            });
-        });
-    }
-  )
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    })
+    user
+      .save()
+      .then(() => {
+        res.status(201).json({
+          message: 'User added successfully!'
+        })
+      })
+      .catch(err => {
+        res.status(500).json({
+          err: err
+        })
+      })
+  })
 }
 
 export function login (req, res, next) {
-
-  User.findOne({email: req.body.email}).then(
-    (user) => {
-        if(!user){
-            return res.status(401).json({
-                error: new Error('User not found')
-            })
-        }
-
-        bcrypt.compare(req.body.password, user.password).then(
-            (valid) => {
-                if(!valid){
-                    return res.status(401).json({
-                        error: new Error('Incorrect password')
-                    })
-                }
-
-                const token = jsonwebtoken.sign(
-                    {userId: user._id},
-                    'RANDOM_TOKEN_SECRET',
-                    {expiresIn: '24h'}
-                )
-
-                res.status(200).json({
-                    userId: user._id,
-                    token: token
-                })
-            }
-        ).catch(err => {
-            res.status(500).json({
-                err: err
-            })
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({
+          error: new Error('User not found').message
         })
-    }
-  ).catch(err => {
-    res.status(500).json({
-        err:err
+      }
+
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({
+              error: new Error('Incorrect password')
+            })
+          }
+
+          const token = jsonwebtoken.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' }
+          )
+
+          res.status(200).json({
+            userId: user._id,
+            token: token
+          })
+        })
+        .catch(err => {
+          res.status(500).json({
+            err: err
+          })
+        })
     })
-  })
+    .catch(err => {
+      res.status(500).json({
+        err: err
+      })
+    })
+}
+
+export function removeUser (req, res, next) {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({
+          error: new Error('User not found').message
+        })
+      }
+
+      User.deleteOne({ email: req.body.email })
+        .then(() => {
+          res.status(200).json({
+            message: 'User deleted successfully'
+          })
+        })
+        .catch(err => {
+          res.status(400).json({
+            err: err
+          })
+        })
+    })
+    .catch(err => {
+      res.status(400).json({
+        err: err.message
+      })
+    })
 }
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { signup, login } from '../controllers/userCtrl.js'
+import { signup, login, removeUser } from '../controllers/userCtrl.js'
 
 const userRouter = express.Router();
 
 userRouter.post('/signup',signup)
 userRouter.post('/login',login)
+userRouter.delete('/remove', removeUser)
 
 
 export default userRouter
